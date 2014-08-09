8080ae6d4bf065355cfe9791238c1b7555da427a
Olivier El Mekki <olivier@el-mekki.com>
2014-08-09 16:41:03 +0200

    FIX turbolinks and merge_requests other actions
    There were two problems with initial release :
    
    1. code would be triggered on all page matching "merge_requests" even if
    they were not merge request creation - like `merge_requests#show` - and
    generated errors
    
    2. gitlab is using turbolinks, so extension code would only be loaded on
    merge creation page if it was a first load or a page reload
    
    Fixed it by binding execution on [page:load
    event](https://github.com/rails/turbolinks#events) and checking if merge
    request creation inputs are present in page.
    
    Since extension may be loaded on any page and merge requests would be
    accessed from there using pjax, we can't just load extension if url
    matches `/merge_requests/`.
    
    Instead, extension will be loaded on any page, and will check if there
    is a `meta[name="description"]` element containing `/GitLab/`.

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-if ( document.querySelectorAll('.commit-row-title').length == 1 ) {
+function Gitlab(){
+  if ( Gitlab.isOnGitlabApp() && this.isMergeRequestForm() ){
+    this.fillForm();
+  }
+}
+
+Gitlab.isOnGitlabApp = function(){
+  var description = document.querySelector( 'meta[name="description"]' );
+  return description && description.content.match( /GitLab/ );
+};
+
+Gitlab.prototype.fillForm = function(){
   var title       = document.querySelector( '.commit-row-title .str-truncated a.commit-row-message' ).innerText;
   var description = document.querySelector( '.commit-row-description pre' ).innerText;
 
   document.querySelector( '#merge_request_title' ).value        = title;
   document.querySelector( '#merge_request_description' ).value  = description;
-}
+};
+
+Gitlab.prototype.isMergeRequestForm = function(){
+  return document.querySelector( '#merge_request_title' ) && document.querySelector( '#merge_request_description' );
+};
+
+
+new Gitlab();
+
+document.addEventListener( 'page:load', function(){
+  new Gitlab();
+}, false );
